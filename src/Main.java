5fe5203c5593e16fa0c3c444b5bcabe07d5c3f13
EWLeo <leontev.ew@gmail.com>
2021-11-02 09:45:03 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketCost = 5889.56;
+        int mileCost = 20;
+        int milesCount = (int)ticketCost/mileCost;
+        System.out.println(milesCount);
+    }
+}
